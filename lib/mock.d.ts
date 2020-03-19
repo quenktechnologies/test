66fbad536c11787ba0d7c72a0a9066025b99399e
Lasana Murray <lmurray@quenk.com>
2020-03-19 08:25:06 -0400

    refactor: update mock api

--- lib/mock.d.ts
+++ lib/mock.d.ts
@@ -1,31 +1,77 @@
 /**
- * Call records the application of a function or method.
+ * Value represents any ES value in a type safe way.
  */
-export declare class Call<A, T> {
+export declare type Value = number | boolean | string | object | Function | Value[];
+/**
+ * ReturnValue of a mocked method or function.
+ *
+ * This can be the raw value or a function that given the arguments of the
+ * method/function will produce a value.
+ */
+export declare type ReturnValue = Value | ValueFun;
+/**
+ * ValueFun
+ */
+export declare type ValueFun = (arg: Value[]) => Value;
+/**
+ * Returns map.
+ */
+export interface Returns {
+    [key: string]: Return;
+}
+/**
+ * Invocation is a recording of method invocations stored by a Mock.
+ */
+export declare class Invocation {
     name: string;
-    args: A[];
-    ret: T;
-    constructor(name: string, args: A[], ret: T);
+    args: Value[];
+    value: ReturnValue;
+    constructor(name: string, args: Value[], value: ReturnValue);
 }
 /**
- * Data recorded during testing.
+ * Return stores a value to be returned by a mocked method.
  */
-export declare class Data {
-    calls: Call<any, any>[];
-    constructor(calls?: Call<any, any>[]);
-    /**
-     * record the application of a method.
-     */
-    record<A, T>(name: string, args: A[], ret: T): T;
-    /**
-     * called returns a list of methods that have been called so far.
-     */
-    called(): string[];
+export declare class Return {
+    name: string;
+    value: ReturnValue;
+    constructor(name: string, value: ReturnValue);
+    get(): Value;
 }
 /**
- * Mock can be extended to satisfy an interface for which we are only interested
- * in recording information about method application.
+ * Mock is a class that can be used to keep track of the mocking of some
+ * interface.
+ *
+ * It provides methods for recording the invocation of methods and setting
+ * their return values. Generally, embedding a Mock instance is preffered to
+ * extending the class.
  */
 export declare class Mock {
-    MOCK: Data;
+    calls: Invocation[];
+    returns: Returns;
+    constructor(calls?: Invocation[], returns?: Returns);
+    /**
+     * invoke records the invocation of a method.
+     * @param method - The method name.
+     * @param args   - An array of arguments the method is called with.
+     * @param ret    - The return value of the method invocation.
+     */
+    invoke<T extends Value>(method: string, args: Value[], ret: T): T;
+    /**
+     * setReturnValue so that invocation of a method always return the desired
+     * result.
+     */
+    setReturnValue(method: string, value: ReturnValue): Mock;
+    /**
+     * getCalledList returns a list of methods that have been called so far.
+     */
+    getCalledList(): string[];
+    /**
+     * wasCalled tests whether a method was called.
+     */
+    wasCalled(method: string): boolean;
+    /**
+     * wasCalledNTimes tests whether a method was called a certain amount of
+     * times.
+     */
+    wasCalledNTimes(method: string, n: number): boolean;
 }
--- src/mock.ts
+++ src/mock.ts
@@ -1,47 +1,136 @@
 
 /**
- * Call records the application of a function or method.
+ * Value represents any ES value in a type safe way.
  */
-export class Call<A, T> {
+export type Value
+    = number
+    | boolean
+    | string
+    | object
+    | Function
+    | Value[]
+    ;
 
-    constructor(public name: string, public args: A[], public ret: T) { }
+/**
+ * ReturnValue of a mocked method or function.
+ *
+ * This can be the raw value or a function that given the arguments of the
+ * method/function will produce a value.
+ */
+export type ReturnValue
+    = Value
+    | ValueFun
+    ;
+
+/**
+ * ValueFun
+ */
+export type ValueFun = (arg: Value[]) => Value;
+
+/**
+ * Returns map.
+ */
+export interface Returns {
+
+    [key: string]: Return
 
 }
 
 /**
- * Data recorded during testing.
+ * Invocation is a recording of method invocations stored by a Mock.
  */
-export class Data {
+export class Invocation {
 
-    constructor(public calls: Call<any, any>[] = []) { }
+    constructor(
+        public name: string,
+        public args: Value[],
+        public value: ReturnValue) { }
+
+}
+
+/**
+ * Return stores a value to be returned by a mocked method.
+ */
+export class Return {
+
+    constructor(public name: string, public value: ReturnValue) { }
+
+    get(): Value {
+
+        return this.value;
+
+    }
+
+}
+
+/**
+ * Mock is a class that can be used to keep track of the mocking of some
+ * interface.
+ *
+ * It provides methods for recording the invocation of methods and setting
+ * their return values. Generally, embedding a Mock instance is preffered to 
+ * extending the class.
+ */
+export class Mock {
+
+    constructor(
+        public calls: Invocation[] = [],
+        public returns: Returns = {}) { }
 
     /**
-     * record the application of a method.
+     * invoke records the invocation of a method.
+     * @param method - The method name.
+     * @param args   - An array of arguments the method is called with.
+     * @param ret    - The return value of the method invocation.
      */
-    record<A, T>(name: string, args: A[], ret: T): T {
+    invoke<T extends Value>(method: string, args: Value[], ret: T): T {
 
-        this.calls.push(new Call(name, args, ret));
-        return ret;
+        this.calls.push(new Invocation(method, args, ret));
+
+        return this.returns.hasOwnProperty(method) ?
+            <T>this.returns[method].get() : ret;
 
     }
 
     /**
-     * called returns a list of methods that have been called so far.
+     * setReturnValue so that invocation of a method always return the desired
+     * result.
      */
-    called(): string[] {
+    setReturnValue(method: string, value: ReturnValue): Mock {
+
+        this.returns[method] = new Return(method, value);
+
+        return this;
+
+    }
+
+    /**
+     * getCalledList returns a list of methods that have been called so far.
+     */
+    getCalledList(): string[] {
 
         return this.calls.map(c => c.name);
 
     }
 
-}
+    /**
+     * wasCalled tests whether a method was called.
+     */
+    wasCalled(method: string): boolean {
 
-/**
- * Mock can be extended to satisfy an interface for which we are only interested
- * in recording information about method application.
- */
-export class Mock {
+        return this.getCalledList().indexOf(method) > - 1;
 
-    MOCK = new Data();
+    }
+
+    /**
+     * wasCalledNTimes tests whether a method was called a certain amount of
+     * times.
+     */
+    wasCalledNTimes(method: string, n: number): boolean {
+
+        return this.getCalledList().reduce((p, c) =>
+            (c === method) ? p + 1 : p, 0) === n;
+
+    }
 
 }
